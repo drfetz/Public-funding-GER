d6c398df30f9aa7308f3e55ba14bedf293712158
Fred Steinmetz <flps@posteo.jp>
2022-12-06 21:10:07 +0100

    markdown and script update

--- scripts/02_explore-data.R
+++ scripts/02_explore-data.R
@@ -6,3 +6,25 @@ aggregate(foerder_db$Fördersumme.in.EUR, by = list(Ressort=foerder_db$Ressort),
 
 aggregate(foerder_db$Fördersumme.in.EUR, by = list(Bundesland=foerder_db$Bundesland.1), FUN=sum)
 
+db <- aggregate(foerder_db$Fördersumme.in.EUR, by = list(Bundesland=foerder_db$Bundesland.1), FUN=sum)
+
+hist(db)
+
+str(db_2)
+
+
+total_funding <- db_2$Foerdermittel
+
+options(scipen = 999)
+format(total_funding, big.mark = ",", scientific = F)
+
+barplot(total_funding,
+        main = "Funding by Federal state",
+        xlab = "Bundesland / Federal state",
+        ylab = "Total funding",
+        font.axis=2,
+        names.arg = db$Bundesland,
+        col = "darkred",
+        horiz = T,
+        las = 1)
+
